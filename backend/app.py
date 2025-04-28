4634c95cdaf50b72ea30a626dd42391ea5789a29
ajarodiy <ajarodiy@asu.edu>
2025-04-28 14:23:44 -0700

    tf format

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ import numpy as np
 from PIL import Image
 import io
 
-model = tf.keras.models.load_model('digit_model.h5')
+model = tf.keras.models.load_model('digit_model')
 app = Flask(__name__)
 CORS(app)
 
