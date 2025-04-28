a09226120c0391562ef9e44a833fafcebdea3e84
ajarodiy <ajarodiy@asu.edu>
2025-04-28 13:53:10 -0700

    requirements.txt

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import tensorflow as tf
 import numpy as np
 from PIL import Image
@@ -6,6 +7,7 @@ import io
 
 model = tf.keras.models.load_model('digit_model.keras')
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
@@ -21,15 +23,22 @@ def predict():
     try:
         img = Image.open(file).convert('L')  # convert to grayscale
         img = img.resize((28, 28))
+        img = Image.fromarray(255 - np.array(img))
         img_array = np.array(img)
         img_array = img_array / 255.0 
         img_array = img_array.reshape(1, 28, 28, 1)
 
         predictions = model.predict(img_array)
         predicted_class = np.argmax(predictions)
-        return jsonify({'prediction': int(predicted_class)})
+
+        confidence = np.max(predictions)
+        if confidence < 0.7:
+            return jsonify({'error': 'Low confidence. Unable to recognize the digit.'})
+        else:
+            return jsonify({'prediction': int(predicted_class)})
 
     except Exception as e:
+        print(str(e))
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
